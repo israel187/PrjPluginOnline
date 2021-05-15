21b6c2033d2bcf381c77fd76388cc153991c4fc2
israel187 <elisabetecassiano2612@gmail.com>
2021-05-16 03:26:11 +0900

    plugins de vibrar notificação e mensagem

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,25 @@
 // This is a JavaScript file
 
+$(document).on("click","#alerta",function(){
+navigator.notification.alert("minha mensagem",null,"Aviso","Aceito");
+});
+
+$(document).on("click","#confirm",function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == 1){
+     navigator.notification.alert("Escolheu a opção A");
+    }else{
+     navigator.notification.alert("Escolheu a opção B");
+    }
+    
+  }
+navigator.notification.confirm("escolha A ou B",confirma,"Escolha:",['A','B']);
+});
+
+$(document).on("click","#beep",function(){
+   navigator.notification.beep(3);
+});
+
+$(document).on("click","#vibrar",function(){
+   navigator.vibrate(40000);
+});
